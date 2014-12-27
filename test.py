2d7619cad80dfae4b5251509d543b0b20d4c0e54
Alan Du <alanhdu@gmail.com>
2014-12-27 16:15:09 -0500

    Add MultiIndex test

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 import pandas as pd
-import blaze as bz
+import numpy as np
 import pandas_format as pf
 
-#df = bz.get_multiindexed_support()
 df = pd.read_csv("/home/alan/workspace/vind/test/iris.csv")
+mi = pd.MultiIndex.from_tuples([(a, b, c) for a in range(5) 
+                                for b in range(5) for c in range(5)])
+mi_df = pd.DataFrame(np.random.rand(125, 5), index=mi)
 
 with open("pandas.html", "w") as fout:
-    fout.write(df.to_html(show_dimensions=True, justify="left"))
+    fout.write(df.to_html(show_dimensions=True, col_space=100))
 
 with open("jinja2.html", "w") as fout:
-    fout.write(pf.to_html(df, show_dimensions=True, justify='left'))
+    fout.write(pf.to_html(df, show_dimensions=True, col_space=100))
