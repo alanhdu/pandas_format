05437b73f6d9bfff363511609bbd1e18b84d3b9b
Alan Du <alanhdu@gmail.com>
2014-12-27 11:56:28 -0500

    More refactoring

--- pandas_format/html.py
+++ pandas_format/html.py
@@ -7,8 +7,35 @@ from pandas.core.common import is_float_dtype
 from pandas.core.config import get_option
 
 from jinja2 import Environment, PackageLoader, Template
-import jinja2
 import markupsafe
+
+def _to_html(df, col_space=None, header=True, index=True, format_value=None,
+             inline=None, sparsify=True, index_names=True, justify=True,
+             bold_rows=True, classes=None, max_rows=float('inf'), 
+             max_cols=float('inf'), show_dimensions=False):
+
+    env = Environment(loader=PackageLoader("pandas_format"),
+                      trim_blocks=True, lstrip_blocks=True)
+    env.filters["format_value"] = format_value
+    def get_rowspan(mi, key, level):
+        count = 0
+        for ts in mi.index.tolist():
+            if ts[level] == key:
+                count += 1
+        return count
+    env.globals.update(get_rowspan=get_rowspan)
+    template = env.get_template("html.tpl")
+
+    if isinstance(df.index, pd.MultiIndex):
+        levels = len(df.index.levels)
+    else:
+        levels = 1
+
+    return template.render(df=df, levels=levels, bold_rows=bold_rows,
+                header=header, col_space=col_space, index=index, 
+                sparsify=sparsify, index_names=index_names,
+                justify=justify, max_rows=max_rows, max_cols=max_cols,
+                show_dimensions=show_dimensions)
 
 def to_html(df, buf=None, columns=None, col_space=None, header=True,
             index=True, na_rep='NaN', formatters=None,
@@ -36,29 +63,13 @@ def to_html(df, buf=None, columns=None, col_space=None, header=True,
         else:
             return r
 
-    def get_rowspan(mi, key, level):
-        count = 0
-        for ts in mi.index.tolist():
-            if ts[level] == key:
-                count += 1
-        return count
+    def inline(row, col):
+        d = {}
+        if col_space is not None and row < 0 or col < 0:
+            d[style] = "minwidth: {};".format(col_space)
 
-    indices = df.index.tolist()
+        return {}
 
-    env = Environment(loader=PackageLoader("pandas_format"),
-                      trim_blocks=True, lstrip_blocks=True)
-    env.filters["format_value"] = format_value
-    env.globals.update(get_rowspan=get_rowspan)
-    jinja2.filters.FILTERS["format_value"] = format_value
-    template = env.get_template("html.tpl")
-
-    if isinstance(df.index, pd.MultiIndex):
-        levels = len(df.index.levels)
-    else:
-        levels = 1
-
-    return template.render(df=df, levels=levels, bold_rows=bold_rows,
-                header=header, col_space=col_space, index=index, 
-                sparsify=sparsify, index_names=index_names,
-                justify=justify, max_rows=max_rows, max_cols=max_cols,
-                show_dimensions=show_dimensions)
+    return _to_html(df, col_space, header, index, format_value, inline, sparsify,
+                    index_names, justify, bold_rows, classes, max_rows,
+                    max_cols, show_dimensions)
