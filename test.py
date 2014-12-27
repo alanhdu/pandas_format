14407884da6636f06bd442cd18f329bce97a685b
Alan Du <alanhdu@gmail.com>
2014-12-27 11:37:22 -0500

    Refactor templates

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ import pandas_format as pf
 #df = bz.get_multiindexed_support()
 df = pd.read_csv("/home/alan/workspace/vind/test/iris.csv")
 
-with open("jinja2.html", "w") as fout:
-    fout.write(df.to_html(show_dimensions=True))
-
 with open("pandas.html", "w") as fout:
-    fout.write(pf.to_html(df, show_dimensions=True))
+    fout.write(df.to_html(show_dimensions=True, justify="left"))
+
+with open("jinja2.html", "w") as fout:
+    fout.write(pf.to_html(df, show_dimensions=True, justify='left'))
